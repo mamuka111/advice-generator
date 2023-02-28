781f85a16f32c98ac648253b6aaa510973dde022
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-02-28 12:25:00 +0400

    finished java script version

---
+++ main.js
@@ -0,0 +1,18 @@
+// let advice = document.querySelector(".advice1")
+let span = document.querySelector(".advice1")
+let button = document.querySelector(".button")
+let span2 = document.querySelector(".span2")
+button.addEventListener("click", () =>{
+ 
+    axios.get(	"https://api.adviceslip.com/advice")
+    .then(function(name){
+        console.log(name.data.slip)
+span2.innerHTML=name.data.slip.id
+span.innerHTML=name.data.slip.advice
+    })
+
+
+})
+// რანდომად აგენერინებს რჩევებს
+
+// axios.get("`https://api.adviceslip.com/advice?random=${randomNum}`").
